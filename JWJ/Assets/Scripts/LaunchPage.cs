4b292ee2c6821a03ece1f8e86061e5747553555d
LouieMarcou <58005859+LouieMarcou@users.noreply.github.com>
2024-03-07 12:57:12 -0600

    Working on Launch Page

--- JWJ/Assets/Scripts/LaunchPage.cs
+++ JWJ/Assets/Scripts/LaunchPage.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LaunchPage : MonoBehaviour
 {
+    private const string IsSceneOpened = "IsSceneOpened";
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (PlayerPrefs.GetInt(IsSceneOpened) == 0)
+        {
+            PlayerPrefs.SetInt(IsSceneOpened, 1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            SceneManager.LoadScene("Loading");
+        }
+        else
+        {
+            SceneManager.LoadScene("JWJ");
+        }
     }
 }
--- JWJ/Assets/Scripts/OnBoard.cs
+++ JWJ/Assets/Scripts/OnBoard.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class OnBoard : MonoBehaviour
 {
+
+    private bool sceneChangeInput = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,13 +14,25 @@ public class OnBoard : MonoBehaviour
 
     IEnumerator LoadYourAsyncScene()
     {
-
+        yield return null;
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("JWJ");
+
+        asyncLoad.allowSceneActivation = false;
 
         // Wait until the asynchronous scene fully loads
         while (!asyncLoad.isDone)
         {
+            if(asyncLoad.progress >= 0.9f && sceneChangeInput)
+            {
+                asyncLoad.allowSceneActivation=true;
+            }
             yield return null;
         }
+    }
+
+
+    public void SetSceneChangeInput()
+    {
+        sceneChangeInput = true;
     }
 }
